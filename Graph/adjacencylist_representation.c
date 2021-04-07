8f3f11a1f030cfea358c68128db4bebe94b1798c
Debarsi Sadhukhan <debarsi2000@gmail.com>
2021-04-07 12:20:40 +0530

    Completed adjacency list representation of graph

--- Graph/adjacencylist_representation.c
+++ Graph/adjacencylist_representation.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 
-struct Vertex{
+struct Vertex{                  //structure of node of vertex
     int info;
     struct Edge* firstEdge;
     struct Vertex* nextVertex;  
@@ -10,7 +10,7 @@ struct Vertex{
 
 typedef struct Vertex Vertex;
 
-struct Edge{
+struct Edge{                    //structure of node of an edge
     struct Vertex* destVertex;
     struct Edge* nextEdge;
 };
@@ -76,9 +76,7 @@ int main()
 
             default:
                 printf("Invalid Choice\n");
-                continue;
-            
-
+               
         }
     }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 }
@@ -96,8 +94,10 @@ Vertex* returnVertex(int v){
     return NULL;
 }
 
+
 /*
 Function to insert a vertex into the list
+If vertex already exist a message will be printed
 If the List is empty the new vertex will be set to head else 
 the new vertex will be pointed as the nextVertex of the now last vertex
 */
@@ -106,28 +106,36 @@ void insertVertex(int u){
     temp->info=u;
     temp->nextVertex=NULL;
     temp->firstEdge=NULL;
-    if(!start){
+    if(!start){                 //if the list is empty set the new vertex as the head
         start=temp;
         return;
     }
     Vertex* it=start;
-    while(it->nextVertex)
+    while(it->nextVertex){      //check if the vertex already exist and also move to the end of the list
+        if(it->info==u)
+            break;
         it=it->nextVertex;
-    
-    it->nextVertex=temp;
+    }
+    if(it->info==u){            //if the vertex already exist show a message
+        free(temp);
+        printf("Vertex already exists\n");
+        return;
+    }
+    it->nextVertex=temp;        //attach the new vertex at the end of the list
 }
 
 /*
 Function to insert an edge to the list.
 The function takes as argument the source and destination vertices.
 The source vertex is found and the destination vertex is inserted into the edge list
+if edge already exist it will print a message
 */
 
-void insertEdge(int dest,int source){
+void insertEdge(int source,int dest){
     Edge* temp=(Edge*)malloc(sizeof(Edge));
     Vertex* sourceVertex=returnVertex(source);
     Vertex* destinationVertex=returnVertex(dest);
-    if(!sourceVertex||!destinationVertex)
+    if(!sourceVertex||!destinationVertex)       //if the vertex of required edges don't exist print a message
         {
             printf("Invalid edge\n");
             return;
@@ -135,68 +143,93 @@ void insertEdge(int dest,int source){
     temp->destVertex=destinationVertex;
     temp->nextEdge=NULL;
     Edge* it=sourceVertex->firstEdge;
-    if(!it)
+    if(!it){                            //if no edges of the source vertex exist set this edge as the firstEdge
         sourceVertex->firstEdge=temp;
-    while(it->nextEdge)
+        return;
+    }
+    while(it->nextEdge){                            //move to the last edge of the edge list of the vertex
+        if(it->destVertex==destinationVertex)       //if the edge already exists break
+            break;
         it=it->nextEdge;
-    it->nextEdge=temp;
+        
+    }
+    if(it->destVertex==destinationVertex)       //if edge already exist print a message
+        {
+            printf("Edge already exist\n");
+            return;
+        }
+    it->nextEdge=temp;                      //set the last edge as the required edge
 }
 
 /*
 Function to delete an edge taking source and destination vertex as parameter
 We find the source vertex and check its list of edges to find the destination vertex and we free that */
 
-void deleteEdge(int dest,int source){
+void deleteEdge(int source,int dest){               
     Vertex* sourceVertex=returnVertex(source);
     Vertex* destinationVertex=returnVertex(dest);
-    if(!sourceVertex||!destinationVertex)
+    if(!sourceVertex||!destinationVertex)               //if the vertex of the edges don't exist print a message
         {
-            printf("Edge Does not exist\n");
+            printf("Vertice of Edge Does not exist\n");
             return;
         }
-    Edge* it=sourceVertex->firstEdge;
-    if(it->destVertex==destinationVertex)
+    Edge* currentEdge=sourceVertex->firstEdge;          
+    if(currentEdge->destVertex==destinationVertex)      //if the firstEdge of the vertex is the required edge
         {
-            sourceVertex->firstEdge=it->nextEdge;
-            free(it);
+            sourceVertex->firstEdge=currentEdge->nextEdge;
+            free(currentEdge);
             return;
         }
-    Edge* it2=it->nextEdge;
-    while(it2->destVertex!=destinationVertex){
-        it=it2;
-        it2=it2->nextEdge;
+    Edge* previousEdge=currentEdge;
+    currentEdge=currentEdge->nextEdge;
+    while(currentEdge->destVertex!=destinationVertex&&!currentEdge){    //exit when destination vertex is found or we reach end of list
+        previousEdge=currentEdge;
+        currentEdge=currentEdge->nextEdge;
     }
-    it->nextEdge=it2->nextEdge;
-    free(it2);
+    if(!currentEdge){                                                   //if the edge is not found print a message
+        printf("Edge does not exist\n");
+        return;
+    }
+    free(currentEdge);                                                  
 }
 
 /*
 Functio to delete incoming Edges of a vertex
 */
 void deleteIncomingEdges(Vertex* source){
+    printf("Inside deleteIncoming\n");
     Vertex* it=start;
-    while(it){
-        if(it==source)
+    while(it){                              //loop to iterate through all vertices
+        if(it==source){                     //we dont need to individually delete incoming vertices from the vertice that is to be deleted since the whole vertex will be deleted later
+            it=it->nextVertex;
             continue;
+        }
         Edge* currentEdge=it->firstEdge;
         Edge* prevEdge=it->firstEdge;
-        if(currentEdge->destVertex==source){
+        if(!currentEdge){                   //if the edge list is empty move to the next vertex              
+            it=it->nextVertex;
+            continue;
+        }
+        if(currentEdge->destVertex==source){    //if the required edge is found at the start free the memory and set firstEdge as the next edge
             it->firstEdge=currentEdge->nextEdge;
             free(currentEdge);
+            it=it->nextVertex;
             continue;
         }
         currentEdge=currentEdge->nextEdge;
-        while(currentEdge->destVertex!=source&&currentEdge!=NULL){
+        while(currentEdge!=NULL&&currentEdge->destVertex!=source){  //keep on iterrating until the list ends or the required edge with destination vertex as the vertex to be deleted is found
             prevEdge=currentEdge;
             currentEdge=currentEdge->nextEdge;
         }
-        if(!currentEdge)
-            return;
-        prevEdge->nextEdge=currentEdge->nextEdge;
+        if(!currentEdge)                            //if edge is not found move to the next vertex
+            {
+                it=it->nextVertex;
+                continue;
+            }
+        prevEdge->nextEdge=currentEdge->nextEdge;   //edge is found free the memory 
         free(currentEdge);
-        
+        it=it->nextVertex;
     }
-    it=it->nextVertex;
 }
 
 /*
@@ -206,13 +239,14 @@ It deletes the vertex in the list and all the edges in other vetrices list*/
 void deleteVertex(int vertexToDelete)
 {
     Vertex* find=returnVertex(vertexToDelete);
-    if(!find){
+    if(!find){                                  //vertex to be deleted does not exist
         printf("Vertex Not Present\n");
         return;
     }
-    deleteIncomingEdges(find);
+    deleteIncomingEdges(find);                  //delete all incoming edges to the vertex
+    
     Vertex* it=start;
-    if(it==vertexToDelete){
+    if(it==find){                               //if vertex is the head of the list make the next vertex as the head and free the memory
         start=it->nextVertex;
         free(it);
         return;
@@ -220,16 +254,29 @@ void deleteVertex(int vertexToDelete)
     Vertex* it2=start;
     it=it->nextVertex;
     while(it){
-        if(it==vertexToDelete){
+        if(it==find){                           //required vertex is found, free the memory
             it2->nextVertex=it->nextVertex;
             free(it);
             return;
         }
+        it2=it;
+        it=it->nextVertex;
     }
 }
 
-void display()
+void display()                                          //display the graph
 {
-    Vertex* it=start;
-    
+    Vertex* iteratorVertex=start;
+    while(iteratorVertex){
+        Edge* iteratorEdge=iteratorVertex->firstEdge;
+        printf("\n%d->\t",iteratorVertex->info);
+        while (iteratorEdge)
+        {
+            printf("%d  ",iteratorEdge->destVertex->info);
+            iteratorEdge=iteratorEdge->nextEdge;
+        }
+        printf("\n");
+        iteratorVertex=iteratorVertex->nextVertex;
+        
+    }
 }
